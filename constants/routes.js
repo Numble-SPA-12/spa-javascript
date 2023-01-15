12ea1a740e8adb29f0037d91be4cff20926b6570
tooooo1 <wer4272@nate.com>
2023-01-16 05:43:09 +0900

    feat: upload page

--- constants/routes.js
+++ constants/routes.js
@@ -1,7 +1,8 @@
 import Main from "../pages/Main";
+import Upload from "../pages/Upload";
 
 export const routes = [
   { path: /^\/$/, element: Main },
   //   { path: /^\/post\/[\w]+$/, element: Post },
-  //   { path: /^\/upload$/, element: Upload },
+  { path: /^\/upload$/, element: Upload },
 ];
