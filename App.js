f8ac6a6e7de6f5bf18b3fca4474ad526648099f6
tooooo1 <wer4272@nate.com>
2023-01-16 02:21:36 +0900

    feat: Main page

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import Header from "./components/Header.js";
+import Main from "./pages/Main.js";
 
 export default function App({ $target }) {
   new Header({ $target });
+  new Main({ $target });
 }
