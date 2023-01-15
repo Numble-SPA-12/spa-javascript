bf73ce3c570740431045f85a0b30d07567546197
tooooo1 <wer4272@nate.com>
2023-01-16 04:28:53 +0900

    refactor: post link

--- components/PostList.js
+++ components/PostList.js
@@ -13,12 +13,14 @@ export default function PostList({ $target, init }) {
         ${this.state
           .map(
             (post) =>
-              `<li class="post_item" key="${post.posdId}">
+              `<li key="${post.postId}">
+                <a class="post_item" href="/post/${post.postId}">
                   <img class="post_img" src="${post.image}" alt="${post.title}">
                   <div class="content">
                     <h1 class="post_title">${post.title}</h1>
                     <p class="post_content">${post.content}</p>
                   </div>
+                </a>
               </li>`
           )
           .join("")}
