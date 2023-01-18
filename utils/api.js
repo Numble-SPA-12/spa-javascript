3ecb1373daff0a1a59a87f59d01320cb873e982b
tooooo1 <wer4272@nate.com>
2023-01-18 18:32:03 +0900

    fix: Content-Security-Policy

--- utils/api.js
+++ utils/api.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
+  headers: {
+    "Content-Security-Policy": "upgrade-insecure-requests",
+  },
 });
 
 const instanceUnsplash = axios.create({
